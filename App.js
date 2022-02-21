00ac12f364e11f0c538be396671f3b8a8df3be5a
casey jackson <ctjacks3@gmail.com>
2022-02-20 20:01:06 -0800

    adds navigation and additinal screens

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen ({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.centerText}>To begin your covid assesment click here</Text>
+      <StatusBar style="auto" />
+
+      <Button
+        title="Go to eval screen"
+        onPress={() => navigation.navigate('EvalScreenOne')}
+        style={{ backgroundColor: 'green' }}>
+        <Text style={{ fontSize: 30, color: 'white' }}>Click here to conitinue</Text>
+      </Button>
+    </View>
+  )
+}
+
+function EvalScreenOne ({navigation}) {
   const [condition, setCondition] = useState('Unknown');
   const [age, setAge] = useState('Unknown');
+
   return (
-    
-    <View style={styles.container}>
+
+  <View style={styles.container}>
       <Text>Select Age Range:</Text>
       <Picker
         selectedValue={age}
@@ -42,11 +61,91 @@ export default function App() {
       <StatusBar style="auto" />
       <Button
         title="Next"
+        onPress={() => navigation.navigate('EvalScreenTwo')}
       />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
-  );
+  )
 }
 
+function EvalScreenTwo ({navigation}){
+  const [numOfPeople, setNumOfPeople] = useState('Unknown');
+  const [destinatoinSize, setDestinationSize] = useState('Unknown');
+  return (
+  <View style={styles.container}>
+      <Text>Select number of people in attendance:</Text>
+      <Picker
+        selectedValue={numOfPeople}
+        onValueChange={(value, index) => setNumOfPeople(value)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Number of People" value=" " />
+        <Picker.Item label="<5" value= "5" />
+        <Picker.Item label="15-25" value="15-25" />
+        <Picker.Item label="25-100" value="25-100" />
+        <Picker.Item label="100-500" value="100-500" />
+        <Picker.Item label="501+" value="501+" />
+      </Picker>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text> Destination Size:</Text>
+      <Picker
+        selectedValue={destinatoinSize}
+        onValueChange={(value, index) => setDestinationSize(value)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Select Destination Size" value=" " />
+        <Picker.Item label="Home" value="extra small" />
+        <Picker.Item label="Small establishment" value="small" />
+        <Picker.Item label="Resturant" value="medium" />
+        <Picker.Item label="Concert" value="large" />
+        <Picker.Item label="Mall" value="extra large" />
+        <Picker.Item label="Outdoors" value="infinite" />
+      </Picker>
+      <StatusBar style="auto" />
+      <Button
+        title="Next"
+        title="Next"
+        onPress={() => navigation.navigate('FinalCalc')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+function FinalCalc ({navigation}) {
+return (
+  <View style={styles.container}>
+      <Text>Your Final assesment is:</Text>
+
+      <StatusBar style="auto" />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+
+      <Button
+        title="Go back to the Home screen"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+)
+}
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="EvalScreenOne" component={EvalScreenOne} />
+        <Stack.Screen name="EvalScreenTwo" component={EvalScreenTwo} />
+        <Stack.Screen name="FinalCalc" component={FinalCalc} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -61,6 +160,11 @@ const styles = StyleSheet.create({
     padding: 5,
     borderWidth: 1,
     borderColor: "black",
+  },
+  centerText:{
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize:30,
   },
 });
 
